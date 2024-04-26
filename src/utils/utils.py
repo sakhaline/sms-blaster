fbf5c73b657629acb79ffa5d4f986d0328d854b4
Nikita Stoliarov <stlrvnk@gmail.com>
2024-04-26 14:25:38 +0300

    Update docstring

--- src/utils/utils.py
+++ src/utils/utils.py
@@ -9,6 +9,7 @@ def format_phone_number(phone_number: str):
     """
     converts phone numbers to a desired format
     (012) 345-6789 -> +10123456789, etc.
+    +380671063839 -> +380671063839 (no change)
     """
     phone_number_formatted = ""
     try:
