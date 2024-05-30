6b5bbd730823341e16ef638806a1a13758967c52
sakhaline <alexandra.ivva@gmail.com>
2024-05-30 15:36:46 +0300

    Schedule

---
+++ token_refresher.py
@@ -0,0 +1,20 @@
+import os
+import time
+import schedule
+from src.logs.logging_config import logger
+from src.auth.ghl_authenticator import refresh_auth_token
+
+
+cwd = os.getcwd()
+
+
+def refresher():
+    secret_path = os.path.join(cwd, "src", "auth", ".ghl_tokens.json")
+    refresh_auth_token(secret_path)
+    logger.info("Token refreshed successfully!!! ^_^")
+
+schedule.every(5).seconds.do(refresher)
+
+while True:
+    schedule.run_pending()
+    time.sleep(1)
