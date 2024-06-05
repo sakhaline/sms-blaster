76fc0d86c0e49afef3e4cf98fe03c69813c820bb
sakhaline <alexandra.ivva@gmail.com>
2024-06-05 17:02:55 +0300

    Load contact to db

--- src/data/messages.py
+++ src/data/messages.py
@@ -1,2 +1,1 @@
 MESSAGE1 = "Hello! Hope you're doing great. Jeff from Land Grit here. I'd like to learn more about your acreages. Is there a good time for a quick conversation? Thanks!"
-# MESSAGE1 = "Hello! Hope you're doing great. It's Jeff. I'd like to learn more about your acreages. Is there a good time for a quick conversation? Thanks!"
